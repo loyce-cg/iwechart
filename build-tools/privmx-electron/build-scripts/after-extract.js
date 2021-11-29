2c1f30dd73000433733f0314bb10cc41d8a5f022
gys <1906192798@qq.com>
2021-11-29 16:28:23 +0800

    1

--- build-tools/privmx-electron/build-scripts/after-extract.js
+++ build-tools/privmx-electron/build-scripts/after-extract.js
@@ -3,7 +3,7 @@ module.exports = function(extractPath, electronVersion, platform, arch, done) {
         const fs = require("fs");
         fs.copyFileSync("../../main/app/README", extractPath + "/README");
         fs.copyFileSync("../../main/app/src/icons/app-icon.png", extractPath + "/resources/app-icon.png");
-        fs.copyFileSync("../../main/privfs-mail-client/app/privmx.desktop", extractPath + "/resources/privmx.desktop");
+        //fs.copyFileSync("../../main/privfs-mail-client/app/privmx.desktop", extractPath + "/resources/privmx.desktop");
     }
     done();
 };
